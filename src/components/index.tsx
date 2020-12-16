b47b5d704a04915b8ab34e2a5733fa4535fa0c44
1109567482@qq.com <cxw2385>
2020-12-16 17:05:25 +0800

    '页面内去掉react-hot-loader/root'

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,8 +1,8 @@
-import { hot } from 'react-hot-loader/root'
+import React from 'react'
 import Home from './Home'
 
 function ComponentIndex() {
   return <Home />
 }
 
-export default hot(ComponentIndex)
+export default ComponentIndex
